56bd5992e5665a9e94d222e8fb067c17645ed735
kabilramar <kabilramar@gmail.com>
2026-05-19 10:53:06 +0530

    Removed sqlite

--- durga/settings.py
+++ durga/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'durga.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,5 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     BASE_DIR/"static"
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
